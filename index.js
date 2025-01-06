065cad69aff1c47bef7629b4ede5fccca42fc334
Kiesha Maria <kiesha.maria@kalvium.community>
2025-01-06 12:20:16 +0530

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+require('dotenv').config();
 const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
+const mongoose=require('mongoose');
 const port = 3010;
+const User = require('./schema.js');
+const connectstring=process.env.MONGO_URI;
+mongoose.connect(connectstring)
+    .then(() => console.log('Connected to database'))
+    .catch((error) => {
+        console.error(`Error connecting to database: ${error.message}`);
+        
+    });
+   
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+    app.use(express.json());
+app.post('/api/users', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    return res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    if (error.name=='ValidationError') {
+      return res.status(400).json({ message: 'Validation error' });
+    }
+    return res.status(500).json({ message: 'Server error' });
+  }
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
